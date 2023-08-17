c14ba72ce08affa41813e19a3ebe7baf05d898f1
hhyyy9@qq.com <huangyu@ivinsight.com>
2023-08-17 09:01:30 +0800

    Add nuget some packages

---
+++ back-end/Data/TripContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_end.Data
+{
+	public class TripContext:DbContext
+	{
+		public TripContext(DbContextOptions options) :base(options)
+		{
+
+		}
+	}
+}
+
---
+++ back-end/Program.cs
@@ -0,0 +1,38 @@
+using back_end.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//MySQL DB connection
+//builder.Services.AddDbContext<TripContext>(opt =>
+//{
+//    opt.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+//        new MySqlServerVersion(new Version(8, 0, 33)));
+//    opt.LogTo(Console.WriteLine, LogLevel.Information);
+//    opt.EnableSensitiveDataLogging();
+//    opt.EnableDetailedErrors();
+//});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
